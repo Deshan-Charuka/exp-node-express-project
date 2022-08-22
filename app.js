1cf1945988e0f42640617becaf7616851aef0d04
Deshan Charuka <d.c.0729439631@gmail.com>
2022-08-22 22:22:36 +0530

    Add middleware functions

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const http = require("http");
 
+//export the express package
 const express = require("express");
 
+//use the e function which is inside in the express package
 const app = express();
+
+//middleware function 01
+app.use((req, res, next) => {
+  console.log("Request handler 01");
+  next();
+});
+//middleware function 02
+app.use((req, res, next) => {
+  console.log("Request handler 02");
+});
 
 const server = http.createServer(app);
 
