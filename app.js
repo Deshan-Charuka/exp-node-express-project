733d673ed7b96e013b935e81afaf0b8715b2f036
Deshan Charuka <d.c.0729439631@gmail.com>
2022-08-22 23:06:26 +0530

    Add response

--- app.js
+++ app.js
@@ -9,11 +9,12 @@ const app = express();
 //middleware function 01
 app.use((req, res, next) => {
   console.log("Request handler 01");
-  next();
+  next();//allows the request to continue next middleware on the line
 });
 //middleware function 02
 app.use((req, res, next) => {
   console.log("Request handler 02");
+  res.send("<h1>Hello from expressJS!</h1>");//sending the response
 });
 
 const server = http.createServer(app);
