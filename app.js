1e2ca168189a01dbc6db9a1a400ed9bc21586af9
Deshan Charuka <d.c.0729439631@gmail.com>
2022-09-07 23:21:51 +0530

    Push remaining changes

--- app.js
+++ app.js
@@ -4,15 +4,22 @@ const express = require("express");
 //use the e function which is inside in the express package
 const app = express();
 
-//middleware function 01
-app.use((req, res, next) => {
+//middleware function 01 | Common middleware (All the requests will go through this..)
+app.use('/',(req, res, next) => {
   console.log("Request handler 01");
   next(); //allows the request to continue next middleware on the line
 });
 //middleware function 02
-app.use((req, res, next) => {
+app.use('/add-product',(req, res, next) => {
   console.log("Request handler 02");
+  res.send("<h1>Add Product Page</h1>"); //sending the response
+});
+
+//middleware function 03
+app.use('/',(req, res, next) => {
+  console.log("Request handler 03");
   res.send("<h1>Hello from expressJS!</h1>"); //sending the response
 });
+
 //newer way of server listening using express
 app.listen(3000);
