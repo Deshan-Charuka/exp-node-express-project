e6d7c02b9cd842991969fd60bf5aaf5403917c56
Deshan Charuka <d.c.0729439631@gmail.com>
2022-09-08 07:49:08 +0530

    add body-parser package to parse incoming requests

--- app.js
+++ app.js
@@ -1,23 +1,31 @@
 //export the express package
 const express = require("express");
+const bodyParser = require("body-parser");
 
 //use the e function which is inside in the express package
 const app = express();
 
-//middleware function 01 | Common middleware (All the requests will go through this..)
-app.use('/',(req, res, next) => {
-  console.log("Request handler 01");
-  next(); //allows the request to continue next middleware on the line
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+
 //middleware function 02
-app.use('/add-product',(req, res, next) => {
-  console.log("Request handler 02");
-  res.send("<h1>Add Product Page</h1>"); //sending the response
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="product"><button type="submit">Add Product</button></form>'
+  ); //sending the response
+});
+
+app.use("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+//middleware function 01 | Common middleware (All the requests will go through this..)
+app.use("/", (req, res, next) => {
+  next(); //allows the request to continue next middleware on the line
 });
 
 //middleware function 03
-app.use('/',(req, res, next) => {
-  console.log("Request handler 03");
+app.use("/", (req, res, next) => {
   res.send("<h1>Hello from expressJS!</h1>"); //sending the response
 });
 
